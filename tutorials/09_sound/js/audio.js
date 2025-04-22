543a851d3fd408b3eb716ba0b0ae8be3bf7c1b53
Andr34G <arellanoga4@gmail.com>
2025-04-22 04:09:48 -0700

    new

---
+++ tutorials/09_sound/js/audio.js
@@ -0,0 +1,10 @@
+
+
+let audio = document.querySelector("#audio");
+
+let playBtn = document.querySelector("#play-button");
+
+playBtn.addEventListener('click', function() {
+    audio.play();
+})
+
